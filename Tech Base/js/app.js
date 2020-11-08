f57be3d477e1dd4cd364e46c1b34a9783b09933f
Huthaifa Salman <62269745+Huthaifa-Dev@users.noreply.github.com>
2020-11-08 17:00:52 +0200

    Website files

---
+++ Tech Base/js/app.js
@@ -0,0 +1,9 @@
+const cards = document.querySelector('.cards .s-wrap');
+const cardArea = document.querySelector('.card-area');
+const books = document.querySelector('.books .s-wrap');
+const bookShelf = document.querySelector('.book-shelf');
+const cont = document.querySelector('.continue .s-wrap');
+
+console.log(cards);
+console.log(books);
+console.log(cont);
